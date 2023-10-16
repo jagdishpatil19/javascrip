ae260af01dbc153de7c832fd94455d02715e131e
jagdishpatil19 <patiljagdish471@gmail.com>
2023-10-16 18:00:26 +0530

    javascript-practise

---
+++ loops.js
@@ -0,0 +1,52 @@
+
+//for loop
+
+// number=[1,2,3,4,4,5,5,4]
+// for (let i=1;i<number; i++) {
+//     console.log(" the number is " ,i,"the odd number is")
+// }
+
+//-------------------------------------------------------------------------------------------------------------
+//for/in loops
+// let obj={
+//     shri:92,
+//     kishore:90,
+//     sham:80,
+//     RN:82
+// }
+
+// for(let a in obj){
+//     console.log( "marks of ",a,"is",obj[a])
+// }
+
+// //---------------------------------------------------------------------------------------------------------
+// //for/of loops
+
+// let loopOf="jagdish"
+
+// for(let b of loopOf){
+//     console.log(loopOf[b])
+// }
+
+// //---------------------------------------------------------------------------------------------------
+// //while loops
+
+// let n =prompt("the number of n")
+// n=Number.parseInt(n)
+ 
+// let i=0;
+// while(i<n){
+//     console.log(i)
+//     i++
+// }
+
+
+
+for(let i=1; i<=20; i++){
+    console.log(i)
+    i=i+1
+}
+
+
+let number1=9999999999999999n
+console.log( typeof number1)
---
+++ string-method.js
@@ -0,0 +1,19 @@
+let itsMe ="     jagdish     ";
+console.log(itsMe.length)
+console.log(itsMe.toUpperCase())
+console.log(itsMe.toLocaleLowerCase())
+console.log(itsMe.slice(2,5))
+console.log(itsMe.replace("jag","sun"))
+console.log(itsMe.valueOf())
+console.log(itsMe.trim())
+console.log(itsMe.trimEnd())
+
+text1 = "Hello"  
+ text2="world"
+// text = "Hello".concat(" ", "World!");
+console.log(text1,text2)
+
+let fName="namman"
+let sName="herry"
+ console.log(`${fName} is friend of ${sName}`)
+console.log( fName,"is friend of", sName)
