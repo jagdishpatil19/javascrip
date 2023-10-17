c4155cf47c1bbdf6e931cccd342f1eee3f59d3bd
jagdishpatil19 <patiljagdish471@gmail.com>
2023-10-17 18:09:05 +0530

    new update

--- loops.js
+++ loops.js
@@ -1,82 +1,78 @@
-// // 
-// // for loop
+// 
+// for loop
 
  
-// for (let i=1;i<10; i++) {
-//     console.log(" the number is " ,i,"the odd number is")
+for (let i=1;i<10; i++) {
+    console.log(" the number is " ,i,"the odd number is")
+}
+
+//-------------------------------------------------------------------------------------------------------------
+//for/in loops
+// let obj={
+//     shri:92,
+//     kishore:90,
+//     sham:80,
+//     RN:82
 // }
 
-// //-------------------------------------------------------------------------------------------------------------
-// //for/in loops
-// // let obj={
-// //     shri:92,
-// //     kishore:90,
-// //     sham:80,
-// //     RN:82
-// // }
+// for(let a in obj){
+//     console.log( "marks of ",a,"is",obj[a])
+// }
 
-// // for(let a in obj){
-// //     console.log( "marks of ",a,"is",obj[a])
-// // }
+// //---------------------------------------------------------------------------------------------------------
+// //for/of loops
 
-// // //---------------------------------------------------------------------------------------------------------
-// // //for/of loops
+// let loopOf="jagdish"
 
-// // let loopOf="jagdish"
+// for(let b of loopOf){
+//     console.log(loopOf[b])
+// }
 
-// // for(let b of loopOf){
-// //     console.log(loopOf[b])
-// // }
+// //---------------------------------------------------------------------------------------------------
+// //while loops
 
-// // //---------------------------------------------------------------------------------------------------
-// // //while loops
-
-// // let n =prompt("the number of n")
-// // n=Number.parseInt(n)
+// let n =prompt("the number of n")
+// n=Number.parseInt(n)
  
-// // let i=0;
-// // while(i<n){
-// //     console.log(i)
-// //     i++
-// // }
-
-
-
-// // for(let i=1; i<=20; i++){
-// //     console.log(i)
-// //     i=i+1
-// // // }
-
-
-// // let number1= [11,2,3,5,7,8,9]
-// //  for (let x =0; x<number1.length;x++){
-// //     console.log(`${number1[x]}"the odd number"`)
-// //  }
-
-// // let w=2
-// // while(w<=10){
-// //     console.log(w,"the number is" )
-// //     w=w*2
-// // }
-
-
-//  let num=[3,5,6,1,30,15]
-// // num.forEach((element)=>{
-// //     console.log(element+element)
-// // })
-
-// // console.log(num)
-// // for(let b of num){
-// //     console.log(b)
-// // }
-// for(j in num){
-//     console.log(num[j])
+// let i=0;
+// while(i<n){
+//     console.log(i)
+//     i++
 // }
 
-// for(f of num){
-//     console.log(f)
+
+
+// for(let i=1; i<=20; i++){
+//     console.log(i)
+//     i=i+1
+// // }
+
+
+// let number1= [11,2,3,5,7,8,9]
+//  for (let x =0; x<number1.length;x++){
+//     console.log(`${number1[x]}"the odd number"`)
+//  }
+
+// let w=2
+// while(w<=10){
+//     console.log(w,"the number is" )
+//     w=w*2
 // }
 
-let a=10
-let b =20
-console.log(a+b)
+
+ let num=[3,5,6,1,30,15]
+// num.forEach((element)=>{
+//     console.log(element+element)
+// })
+
+// console.log(num)
+// for(let b of num){
+//     console.log(b)
+// }
+for(j in num){
+    console.log(num[j])
+}
+
+for(f of num){
+    console.log(f)
+}
